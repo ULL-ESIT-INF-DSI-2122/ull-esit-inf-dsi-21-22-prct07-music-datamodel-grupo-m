7fd8776952b42862aa896c904bb3bd19a6de0931
Joel Escobar <alu0101130408@ull.edu.es>
2022-04-11 21:35:41 +0000

    modificado la estructura de la carpeta test

--- tests/Basic_Class_Test/album.spec.ts
+++ tests/Basic_Class_Test/album.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import {expect} from 'chai';
-import {data} from "../src/data";
-import {albumCollection, artistCollection, groupCollection} from '../src/collection';
+import {data} from "../../src/data";
+import {albumCollection, artistCollection, groupCollection} from '../../src/collection';
 // //import {DataBase, albumCollection, artistCollection, groupCollection, genreCollection, songCollection, playlistCollection} from './database'
 
 
